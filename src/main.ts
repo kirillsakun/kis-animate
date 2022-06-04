9aa6e6b712ba2a085dd7bc95b3dac8d6495c5270
kirillsakun <kirillsakun03@gmail.com>
2022-06-04 21:47:23 +0400

    styles: add headings styles

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import KisAnimate from './animate.ts';
 
 
 const examples = new KisAnimate({
-	items: '.heading',
+	items: '.heading.heading--lg',
 	defaultType: 'fade-in',
 	defaultDelay: 100,
 	defaultDuration: 500,
